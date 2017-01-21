2d887afa557a5d7b1fecf63638dddb023e731230
jvbeethoven <julievanbeethoven@gmail.com>
2017-01-21 20:33:15 +0100

    :lipstick: Hardcoded homepage items + did basic responsive styling see #7 #24 #5 #6

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 import handleMouseMoveDocument from './lib/HandleMouseMoveDocument';
 
 const init = () => {
+  console.log(`hello`);
 
   const $img = document.querySelector(`.header-title`);
   handleMouseMoveDocument({element: $img, xPos: 9, yPos: 10});
